9e42df4e869441e381f981747bc30d42211d13f4
evevalentina <kkq506@york.ac.uk>
2025-04-02 13:01:28 +0100

    Create tempory test file for formative.py
    creating a tempory test file to test lines of code

---
+++ python/tempory test file for formative.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  2 12:53:50 2025
+
+@author: evepl
+"""
+
+#Create a tempory new test function 
+def test_my_function(def list_tasks():
+    
+    with open(TASK_FILE, "r", encoding= "utf-8") as file:
+        tasks = file.readlines()
+        counter = 1
+        tasks in tasks:
+            output_string = output_string + sri(counter) + ","+tasks
+            counter = counter + 1): 
+    assert my_function(input)
+    print("Test passed!")
+#Run my test 
+test_my_function()
